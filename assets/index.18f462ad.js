ed98da26d2fea828f90275b9926cd2d4fe630458
zc <zhuc@connor.net.cn>
2022-01-24 15:13:15 +0800

    element-plus二次封装

---
+++ assets/index.18f462ad.js
@@ -0,0 +1,1 @@
+import{m as t}from"./index.d6fa12db.js";import{d as a,r as l,e as n,b as i,o as r}from"./vendor.d97595ad.js";const f=a({setup(d){const e=l(!1);return(u,o)=>(r(),n("div",null,[i(t,{title:"\u9009\u62E9\u56FE\u6807",visible:e.value,"onUpdate:visible":o[0]||(o[0]=s=>e.value=s)},null,8,["visible"])]))}});export{f as default};
